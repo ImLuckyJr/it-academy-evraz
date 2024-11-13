976ec2f7f8a1a804f6e0821410ab2ae07fb72dd9
Pavel <im.lucky.jr@gmail.com>
2024-11-13 19:33:54 +0500

    Описание задач

--- web/frontend/4_js_инпуты_кнопки/шаблон 1 задача/js.js
+++ web/frontend/4_js_инпуты_кнопки/шаблон 1 задача/js.js
@@ -1,13 +1,8 @@
-let body = document.getElementById('body');
-
 function makeRed() {
-    body.style.backgroundColor = 'red';
 }
 
 function makeBlue() {
-    body.style.backgroundColor = 'blue';
 }
 
 function makeGreen() {
-    body.style.backgroundColor = 'green';
 }
